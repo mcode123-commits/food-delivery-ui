2059d9ffc5413f3d1c860a0cb0a621561855e980
mankusmichal <mankusmichal@gmail.com>
2025-11-08 20:40:40 +0100

    food catalog component added

---
+++ src/app/food-catalog/component/food-catalog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-catalog',
+  imports: [],
+  templateUrl: './food-catalog.html',
+  styleUrl: './food-catalog.css',
+})
+export class FoodCatalog {
+
+}
